1d1752b50ee8749a9edbe83f40099e5a5c4fdf05
mrqwer <qlzpam45@gmail.com>
2022-05-30 11:16:00 +0600

    added inproj

---
+++ finalweb/src/main/java/com/example/finalweb/FinalwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalwebApplication.class, args);
+    }
+
+}
