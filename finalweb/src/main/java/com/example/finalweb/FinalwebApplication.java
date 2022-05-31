8a66d7c86fe30c2e9c8770e7f5991f21aa4fa27a
mrqwer <qlzpam45@gmail.com>
2022-05-31 14:33:26 +0600

    nothing

--- finalweb/src/main/java/com/example/finalweb/FinalwebApplication.java
+++ finalweb/src/main/java/com/example/finalweb/FinalwebApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FinalwebApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FinalwebApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(FinalwebApplication.class, args);
+	}
 
 }
